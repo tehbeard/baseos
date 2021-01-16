4bc99466ad5f94e568aa36f2475768651c24c725
tehbeard <tehbeard@gmail.com>
2021-01-16 09:14:25 +0000

    Switch to CC:Tweaked inventory commands

--- bin/api/invlist.lua
+++ bin/api/invlist.lua
@@ -17,27 +17,18 @@ function split(inputstr, sep)
         return t
 end
 
-function findSlot(item)
-  id,dmg = unpack(split(item,","))
-  t = p.getInventorySize()
+function findSlot(id)
+  t = p.size()
   a = 1
   while a < t+1 do
-    s = p.getStackInSlot(a)
+    s = p.getItemDetail(a)
     if s ~= nil then
       if (
-           s.id == id and 
-           ( 
-             (
-               dmg ~= nil and 
-               s.dmg == tonumber(dmg) 
-             ) or 
-             dmg == nil
-           ) 
+           s.name == id 
          ) or 
          ( 
-           s.ore_dict ~= nil and 
-           s.ore_dict[id] 
-           and dmg == nil 
+           s.tags ~= nil and 
+           s.tags[id] 
          ) then
         return a
       end
@@ -47,38 +38,37 @@ function findSlot(item)
   return nil
 end
 
-function invlist.countItem(item)
-  id,dmg = unpack(split(item,","))
-  t = p.getInventorySize()
+function invlist.countItem(id)
+  t = p.size()
   a = 1
-  qty = 0
+  count = 0
   while a < t+1 do
-    s = p.getStackInSlot(a)
+    s = p.getItemDetail(a)
     if s ~= nil then
-      if (s.id == id and ( (dmg ~= nil and s.dmg == tonumber(dmg) ) or dmg == nil) ) or ( s.ore_dict ~= nil and s.ore_dict[id] and dmg == nil ) then
-        qty = qty + s.qty
+      if (s.name == id ) or ( s.tags ~= nil and s.tags[id] ) then
+        count = count + s.count
       end
     end
     a = a + 1  
   end
-  return qty
+  return count
 end
 
 function invlist.list()
   r = {}
-  t = p.getInventorySize()
+  t = p.size()
   a = 1
   while a < t+1 do
-    s = p.getStackInSlot(a)
+    s = p.getItemDetail(a)
     if s ~= nil then
-      local tag = s.id .. "," .. tostring(s.dmg)
+      local tag = s.name
       if r[tag] == nil then
         r[tag] = {
-          name = s.display_name,
-          qty = 0
+          name = s.displayName,
+          count = 0
         }
       end
-        r[tag].qty = r[tag].qty + s.qty
+        r[tag].count = r[tag].count + s.count
     end
     a = a + 1  
   end
@@ -86,14 +76,17 @@ function invlist.list()
 end
 
 
-function invlist.fetchItem(item, to, qty)
-  if qty == nil then
-    qty = 1
+function invlist.fetchItem(item, to, count)
+  if count == nil then
+    count = 1
   end
   slot = findSlot(item)
   if slot ~= nil then
     print("Found in slot " .. tostring(slot))
-    return p.pushItemIntoSlot(_DIR, slot, qty, to) > 0
+    found = p.pushItems("top", slot, count) > 0
+    turtle.select(to)
+    turtle.suckUp(count)
+    return found
   else
     return false
   end
